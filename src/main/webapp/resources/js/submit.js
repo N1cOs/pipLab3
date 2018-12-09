1009b79db1e40ff8cd9d6fac5c5304f3aa77c26e
Nick <nickkarmatskikh@gmail.com>
2018-12-10 01:41:53 +0300

    do nothing

--- src/main/webapp/resources/js/submit.js
+++ src/main/webapp/resources/js/submit.js
@@ -6,6 +6,7 @@ document.getElementById("myCanvas").addEventListener('click', function (event) {
     y_input.value = (-coordsWithOffset.y + canvas.height / 2) / 100;
     x_input.setAttribute('checked', 'true');
     // document.getElementById('form:sendButton').click();
+});
 
 
 function getWithOffset(canvas, event) {
